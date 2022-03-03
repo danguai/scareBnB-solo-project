fcfc63412b8783f8c5c75b91371207b9de8ae52a
Daniel Blanco <db@d-blanco.com>
2022-03-03 12:39:21 -0500

    API_Routes - DONE

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function (req, res) {
+const apiRouter = require('./api');
+
+router.use('./api', apiRouter);
+
+router.get('/hello/world', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
