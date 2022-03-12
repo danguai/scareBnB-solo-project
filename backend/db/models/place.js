67fd3ff9e67aabb343ea741a19515dc4130171ac
Daniel Blanco <db@d-blanco.com>
2022-03-11 19:15:35 -0500

    AllPlaces css

--- backend/db/models/place.js
+++ backend/db/models/place.js
@@ -3,14 +3,25 @@
 module.exports = (sequelize, DataTypes) => {
 
   const Place = sequelize.define('Place', {
+    title: DataTypes.STRING,
     address: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING,
     country: DataTypes.STRING,
     zipcode: DataTypes.STRING,
+    url_image_01: DataTypes.TEXT,
+    url_image_02: DataTypes.TEXT,
+    url_image_03: DataTypes.TEXT,
+    url_image_04: DataTypes.TEXT,
+    url_image_05: DataTypes.TEXT,
+    amenities_01: DataTypes.TEXT,
+    amenities_02: DataTypes.TEXT,
+    amenities_03: DataTypes.TEXT,
+    amenities_04: DataTypes.TEXT,
+    amenities_05: DataTypes.TEXT,
     price: DataTypes.STRING,
     rating: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
+    userId: DataTypes.INTEGER,
   }, {});
 
   Place.associate = function (models) {
